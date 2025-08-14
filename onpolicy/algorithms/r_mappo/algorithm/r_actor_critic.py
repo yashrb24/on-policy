8d5d95141df2815476526ba932ce09aa07e1c0dd
Yash Bhisikar <bhisikaryash2403@gmail.com>
2025-08-14 23:46:46 +0530

    feat: replace CNNBase and MLPBase with TransformerEncoderBase in R_Actor and R_Critic classes

--- onpolicy/algorithms/r_mappo/algorithm/r_actor_critic.py
+++ onpolicy/algorithms/r_mappo/algorithm/r_actor_critic.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from onpolicy.algorithms.utils.util import init, check
 from onpolicy.algorithms.utils.cnn import CNNBase
 from onpolicy.algorithms.utils.mlp import MLPBase
+from onpolicy.algorithms.utils.transformer_encoder import TransformerEncoderBase
 from onpolicy.algorithms.utils.rnn import RNNLayer
 from onpolicy.algorithms.utils.act import ACTLayer
 from onpolicy.algorithms.utils.popart import PopArt
@@ -30,8 +31,9 @@ class R_Actor(nn.Module):
         self.tpdv = dict(dtype=torch.float32, device=device)
 
         obs_shape = get_shape_from_obs_space(obs_space)
-        base = CNNBase if len(obs_shape) == 3 else MLPBase
-        self.base = base(args, obs_shape)
+        # base = CNNBase if len(obs_shape) == 3 else MLPBase
+        # self.base = base(args, obs_shape)
+        self.base = TransformerEncoderBase(args, obs_shape)
 
         if self._use_naive_recurrent_policy or self._use_recurrent_policy:
             self.rnn = RNNLayer(self.hidden_size, self.hidden_size, self._recurrent_N, self._use_orthogonal)
@@ -137,8 +139,9 @@ class R_Critic(nn.Module):
         init_method = [nn.init.xavier_uniform_, nn.init.orthogonal_][self._use_orthogonal]
 
         cent_obs_shape = get_shape_from_obs_space(cent_obs_space)
-        base = CNNBase if len(cent_obs_shape) == 3 else MLPBase
-        self.base = base(args, cent_obs_shape)
+        # base = CNNBase if len(cent_obs_shape) == 3 else MLPBase
+        # self.base = base(args, cent_obs_shape)
+        self.base = TransformerEncoderBase(args, cent_obs_shape)
 
         if self._use_naive_recurrent_policy or self._use_recurrent_policy:
             self.rnn = RNNLayer(self.hidden_size, self.hidden_size, self._recurrent_N, self._use_orthogonal)
