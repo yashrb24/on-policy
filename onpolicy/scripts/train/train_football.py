755b78d5821abd250f574b57003901a71b5de9c4
Yash Bhisikar <bhisikaryash2403@gmail.com>
2025-08-14 23:39:00 +0530

    fix: env resolution from remote system + script update with wandb acc

--- onpolicy/scripts/train/train_football.py
+++ onpolicy/scripts/train/train_football.py
@@ -135,7 +135,7 @@ def main(args):
     if all_args.use_wandb:
         run = wandb.init(config=all_args,
                          project=all_args.env_name,
-                         entity=all_args.wandb_name,
+                         entity=all_args.user_name,
                          notes=socket.gethostname(),
                          name="-".join([
                             all_args.algorithm_name,
