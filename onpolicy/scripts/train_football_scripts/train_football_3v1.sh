ccbee0981fc9eda5c0412d8e9e6be665ba25bdb3
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2025-09-04 16:09:22 +0100

    Update train_football_3v1.sh

--- onpolicy/scripts/train_football_scripts/train_football_3v1.sh
+++ onpolicy/scripts/train_football_scripts/train_football_3v1.sh
@@ -17,4 +17,5 @@ CUDA_VISIBLE_DEVICES=0 python ../train/train_football.py \
 --num_agents ${num_agents} --num_env_steps ${num_env_steps} --episode_length ${episode_length} \
 --representation "simple115v2" --rewards "scoring" --n_rollout_threads 50 --ppo_epoch 15 --num_mini_batch 2 \
 --save_interval 200000 --log_interval 200000 --use_eval --eval_interval 400000 --n_eval_rollout_threads 100 --eval_episodes 100 \
---use_transformer_base_actor --use_comms_channel --comm_coeff 0.0001 --user_name "yashrb" --wandb_name "on-policy" 
+--use_transformer_base_actor --user_name "yashrb" --wandb_name "on-policy" 
+# --use_comms_channel --comm_coeff 0.0001
