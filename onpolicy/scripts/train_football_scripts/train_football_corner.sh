042c9d914fc3ad04ca80638c75483acfc6465619
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2025-09-04 16:08:48 +0100

    Update train_football_corner.sh

--- onpolicy/scripts/train_football_scripts/train_football_corner.sh
+++ onpolicy/scripts/train_football_scripts/train_football_corner.sh
@@ -18,6 +18,6 @@ echo "n_rollout_threads: ${n_rollout_threads} \t ppo_epoch: ${ppo_epoch} \t num_
 CUDA_VISIBLE_DEVICES=0 python ../train/train_football.py \
 --env_name ${env} --scenario_name ${scenario} --algorithm_name ${algo} --experiment_name ${exp} --seed ${seed} \
 --num_agents ${num_agents} --num_env_steps ${num_env_steps} --episode_length ${episode_length} \
---representation "simple115v2" --rewards "scoring,checkpoints" --n_rollout_threads 50 --ppo_epoch 15 --num_mini_batch 2 \
+--representation "simple115v2" --rewards "scoring" --n_rollout_threads 50 --ppo_epoch 15 --num_mini_batch 2 \
 --save_interval 200000 --log_interval 200000 --use_eval --eval_interval 400000 --n_eval_rollout_threads 100 --eval_episodes 100 \
 --user_name "yashrb" --wandb_name "on-policy" 
