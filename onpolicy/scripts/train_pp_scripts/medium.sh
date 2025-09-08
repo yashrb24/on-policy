88bea424e042da719b5df20ce872a9976121ac28
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2025-09-08 16:37:25 +0100

    Update medium.sh

--- onpolicy/scripts/train_pp_scripts/medium.sh
+++ onpolicy/scripts/train_pp_scripts/medium.sh
@@ -11,13 +11,13 @@ num_agents=5
 num_env_steps=3000000
 episode_length=40
 
-CUDA_VISIBLE_DEVICES=0 python /Users/yashrb/Projects/on-policy/onpolicy/scripts/train/train_predatorprey.py \
+CUDA_VISIBLE_DEVICES=0 python ../train/train_predatorprey.py \
 --env_name ${env} --scenario_name ${scenario} --algorithm_name ${algo} --experiment_name ${exp} --seed 1 \
 --num_agents ${num_agents} --num_env_steps ${num_env_steps} --episode_length ${episode_length} \
- --dim 7 --vision 1 \
---n_rollout_threads 15 --ppo_epoch 15 --num_mini_batch 1 \
+ --dim 10 --vision 1 \
+--n_rollout_threads 15 --ppo_epoch 10 --num_mini_batch 1 \
 --save_interval 200 --log_interval 400 \
 --use_transformer_base_actor \
---use_wandb False
+--use_wandb True --user_name "yashrb" --wandb_name "on-policy"
 #--use_wandb False
-#--user_name "yashrb" --wandb_name "on-policy"
+
