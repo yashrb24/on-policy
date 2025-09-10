bf43421a281d656c7c2ee0e3357dc08a181074e2
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2025-09-10 12:28:42 +0100

    Update medium.sh

--- onpolicy/scripts/train_pp_scripts/medium.sh
+++ onpolicy/scripts/train_pp_scripts/medium.sh
@@ -18,6 +18,6 @@ CUDA_VISIBLE_DEVICES=0 python ../train/train_predatorprey.py \
 --n_rollout_threads 15 --ppo_epoch 10 --num_mini_batch 1 \
 --save_interval 200 --log_interval 400 \
 --use_transformer_base_actor \
---use_wandb True --user_name "yashrb" --wandb_name "on-policy"
+--user_name "yashrb" --wandb_name "on-policy"
 #--use_wandb False
 
