089486bae9e2286ad4f7562f708e4ee9de806066
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2025-09-12 17:00:30 +0100

    Update train_pp_hard_parallel.sh

--- onpolicy/scripts/train_pp_scripts/train_pp_hard_parallel.sh
+++ onpolicy/scripts/train_pp_scripts/train_pp_hard_parallel.sh
@@ -37,7 +37,7 @@ BEST_N_HEAD="4"
 BEST_HIDDEN_SIZE="64"  # Must match n_embd
 
 # DDCL 1e-4/ 1e-3/ 1e-2
-COMM_COEFF="1e-3"
+COMM_COEFF="1e-2"
 # --use_comms_channel \
 # --comm_coeff "$COMM_COEFF" \
 
