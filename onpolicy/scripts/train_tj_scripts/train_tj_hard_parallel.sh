a49b2eacb83be0b943359b57074b82a2f42d1a39
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2025-09-15 01:20:13 +0100

    Update train_tj_hard_parallel.sh

--- onpolicy/scripts/train_tj_scripts/train_tj_hard_parallel.sh
+++ onpolicy/scripts/train_tj_scripts/train_tj_hard_parallel.sh
@@ -39,7 +39,7 @@ BEST_N_HEAD="4"
 BEST_HIDDEN_SIZE="64"  # Must match n_embd
 
 # DDCL 1e-4/ 1e-3/ 1e-2
-COMM_COEFF="1e-4"
+COMM_COEFF="1e-3"
 # --use_comms_channel \
 # --comm_coeff "$COMM_COEFF" \
 
@@ -112,7 +112,7 @@ echo ""
 get_gpu_for_job() {
     local job_index=$1
     # local gpu_index=$((job_index % NUM_GPUS))
-    local gpu_index=2
+    local gpu_index=3
     echo "${AVAILABLE_GPUS[$gpu_index]}"
 }
 
