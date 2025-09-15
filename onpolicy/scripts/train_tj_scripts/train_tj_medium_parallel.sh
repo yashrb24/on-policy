93cb06ff75a4b7de1b289db2a11528ac94c9e8c7
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2025-09-15 01:31:38 +0100

    Update train_tj_medium_parallel.sh

--- onpolicy/scripts/train_tj_scripts/train_tj_medium_parallel.sh
+++ onpolicy/scripts/train_tj_scripts/train_tj_medium_parallel.sh
@@ -12,7 +12,7 @@ ENV_NAME="TrafficJunction"
 SCENARIO_NAME="medium"
 ALGORITHM_NAME="rmappo"
 # "tj-medium-best-config"/ "tj-medium-ddcl-config"/ "tj-medium-fakequant-config"
-EXPERIMENT_NAME="tj-medium-ddcl-config"
+EXPERIMENT_NAME="tj-medium-fakequant-config"
 NUM_AGENTS=5
 NUM_ENV_STEPS=3000000
 EPISODE_LENGTH=40
@@ -39,12 +39,12 @@ BEST_N_HEAD="4"
 BEST_HIDDEN_SIZE="64"  # Must match n_embd
 
 # DDCL 1e-4/ 1e-3/ 1e-2
-COMM_COEFF="1e-2"
+# COMM_COEFF="1e-2"
 # --use_comms_channel \
 # --comm_coeff "$COMM_COEFF" \
 
 # FAKEQUANT 4/8/16
-# QUANT_BITS="8"
+QUANT_BITS="8"
 # --use_fake_quantization \
 # --quant_bits "$QUANT_BITS" \
 
@@ -170,8 +170,8 @@ run_seed_experiment() {
             --n_block "$BEST_N_BLOCK" \
             --n_embd "$BEST_N_EMBD" \
             --n_head "$BEST_N_HEAD" \
-            --use_comms_channel \
-            --comm_coeff "$COMM_COEFF" \
+            --use_fake_quantization \
+            --quant_bits "$QUANT_BITS" \
             --user_name "$WANDB_USER" \
             --wandb_name "$WANDB_PROJECT" \
             2>&1 | tee -a "$log_file"
