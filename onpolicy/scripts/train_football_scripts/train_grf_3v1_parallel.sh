339669061b7c15fce1cf2a19981f75e7dbe1e8b3
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2025-09-15 15:22:16 +0100

    Update train_grf_3v1_parallel.sh

--- onpolicy/scripts/train_football_scripts/train_grf_3v1_parallel.sh
+++ onpolicy/scripts/train_football_scripts/train_grf_3v1_parallel.sh
@@ -13,9 +13,9 @@ SCENARIO_NAME="academy_3_vs_1_with_keeper"
 ALGORITHM_NAME="rmappo"
 # "grf-3v1-best-config"/ "grf-3v1-ddcl-config"/ "grf-3v1-fakequant-config"
 EXPERIMENT_NAME="grf-3v1-fakequant-config"
-NUM_AGENTS=5
-NUM_ENV_STEPS=3000000
-EPISODE_LENGTH=40
+NUM_AGENTS=3
+NUM_ENV_STEPS=15000000
+EPISODE_LENGTH=200
 DIM=10
 VISION=1
 N_ROLLOUT_THREADS=15
